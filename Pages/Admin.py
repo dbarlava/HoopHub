9e7ceef00698812ca62366528e107a79d5adf58a
dbarlava <158539258+dbarlava@users.noreply.github.com>
2025-11-23 21:53:06 -0800

    Enhance player management features in Admin.py
    Updated player insertion and modification forms to include unique keys for each input field. Added functionality to change player details including team, age, position, number, height, and weight.

--- Pages/Admin.py
+++ Pages/Admin.py
@@ -105,14 +105,14 @@ with col2:
 st.divider()
 
 with st.expander("Insert a New Player"):
-    player_team = st.selectbox("Team:", options = db.get_teams(), index = None, placeholder = "Select a team")
+    player_team = st.selectbox("Team:", options = db.get_teams(), index = None, placeholder = "Select a team", key = "insert_player_team")
     player_first_name = st.text_input("First Name:")
     player_last_name = st.text_input("Last Name:")
-    player_age = st.number_input("Age:", min_value = 18, max_value = 60)
-    player_position = st.text_input("Position (e.g. G, F, C, F-C, G-F):")
-    player_number = st.number_input("Number:", min_value = 0, max_value = 99)
-    player_height = st.number_input("Height (inches):", min_value = 60)
-    player_weight = st.number_input("Weight (lbs):", min_value = 100)
+    player_age = st.number_input("Age:", min_value = 18, max_value = 60, key = "insert_player_age")
+    player_position = st.selectbox("Position:", options = db.get_positions(), index = None, placeholder = "Select a position", key = "insert_player_position")
+    player_number = st.number_input("Number:", min_value = 0, max_value = 99, key = "insert_player_number")
+    player_height = st.number_input("Height (inches):", min_value = 60, key = "insert_player_height")
+    player_weight = st.number_input("Weight (lbs):", min_value = 100, key = "insert_player_weight")
     if st.button("Insert Player"):
         if not player_team or not player_first_name or not player_last_name or not player_age or not player_position or not player_height or not player_weight:
             st.error("Please fill all required fields.")
@@ -131,3 +131,111 @@ with st.expander("Insert a New Player"):
                 st.success("Player inserted successfully!")
             else:
                 st.error("Failed to insert player.")
+
+with st.expander("Change Player Details"):
+    player_name = st.selectbox("Player:", options = db.get_players(), index = None, placeholder = "Select a player")
+    if player_name:
+        st.markdown(f"### Editing Player: {player_name}")
+        
+        col1, col2, col3 = st.columns([1, 2, 2])
+        with col1:
+            st.write("Team:")
+        with col2:
+            player_team = st.selectbox("", options = db.get_teams(), index = None, placeholder = "Select a team", key = "change_player_team", label_visibility = "collapsed")
+        with col3:
+            if st.button("Change Player Team"):
+                if not player_name or not player_team:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_team(player_name, player_team)
+                    if success:
+                        st.success("Player team changed successfully!")
+                    else:
+                        st.error("Failed to change player team.")
+        col4, col5, col6 = st.columns([1, 2, 2])
+        with col4:
+            st.write("Age:")
+        with col5:
+            player_age = st.number_input("", min_value = 18, max_value = 60, value = None, key = "change_player_age", label_visibility = "collapsed")
+        with col6:
+            if st.button("Change Player Age"):
+                if not player_name or not player_age:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_age(player_name, player_age)
+                    if success:
+                        st.success("Player age changed successfully!")
+                    else:
+                        st.error("Failed to change player age.")
+        col7, col8, col9 = st.columns([1, 2, 2])
+        with col7:
+            st.write("Position:")
+        with col8:
+            player_position = st.selectbox("", options = ['G', 'F', 'C', 'F-C', 'G-F'], index = None, placeholder = "Select a position", key = "change_player_position", label_visibility = "collapsed")
+        with col9:
+            if st.button("Change Player Position"):
+                if not player_name or not player_position:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_position(player_name, player_position)
+                    if success:
+                        st.success("Player position changed successfully!")
+                    else:
+                        st.error("Failed to change player position.")
+        col10, col11, col12 = st.columns([1, 2, 2])
+        with col10:
+            st.write("Number:")
+        with col11:
+            player_number = st.number_input("", min_value = 0, max_value = 99, value = None, key = "change_player_number", label_visibility = "collapsed")
+        with col12:
+            if st.button("Change Player Number"):
+                if not player_name or not player_number:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_number(player_name, player_number)
+                    if success:
+                        st.success("Player number changed successfully!")
+                    else:
+                        st.error("Failed to change player number.")
+        col13, col14, col15 = st.columns([1, 2, 2])
+        with col13: 
+            st.write("Height (inches):")
+        with col14:
+            player_height = st.number_input("", min_value = 60, value = None, key = "change_player_height", label_visibility = "collapsed")
+        with col15:
+            if st.button("Change Player Height"):
+                if not player_name or not player_height:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_height(player_name, player_height)
+                    if success:
+                        st.success("Player height changed successfully!")
+                    else:
+                        st.error("Failed to change player height.")
+
+        col16, col17, col18 = st.columns([1, 2, 2])
+        with col16:
+            st.write("Weight (lbs):")
+        with col17:
+            player_weight = st.number_input("", min_value = 100, value = None, key = "change_player_weight", label_visibility = "collapsed")
+        with col18:
+            if st.button("Change Player Weight"):
+                if not player_name or not player_weight:
+                    st.error("Please fill all required fields.")
+                else:
+                    success = db.change_player_weight(player_name, player_weight)
+                    if success:
+                        st.success("Player weight changed successfully!")
+                    else:
+                        st.error("Failed to change player weight.")
+
+with st.expander("Delete a Player"):
+    player_name = st.selectbox("Player:", options = db.get_players(), index = None, placeholder = "Select a player", key = "delete_player_name")
+    if player_name:
+        st.markdown(f"### Deleting Player: {player_name}")
+        if st.button("Delete Player"):
+            success = db.delete_player(player_name)
+            if success:
+                st.success("Player deleted successfully!")
+            else:
+                st.error("Failed to delete player.")
